8f38994c85f3c894e1aad9e3baf88f0adeb5ff2f
Ayush <ayush9783@gmail.com>
2025-07-04 15:23:46 +0530

    header section completed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
-
 <template>
-  <h1>Hello from Vue + Bun!</h1>
+  <div>
+    <div class="first-div">Welcome To Scissors & Style</div>
+
+    <the-header />
+    <h1>Hello from Vue + Bun!</h1>
+  </div>
 </template>
 
-<script setup>
+<script>
+import TheHeader from "./components/Header/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-h1 {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #c0c0c0;
+}
+</style>
+
+<style scoped>
+.first-div {
+  background-color: #f6c3aa;
+  display: flex;
+  justify-content: center;
+  padding: 13px 0;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  font-size: 13px;
 }
 </style>
---
+++ src/components/Header/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="nav">
+    <div class="none">
+      <img src="../../assets/logo.png" alt="" class="nav-img" />
+      <p class="title">Scissors & Style</p>
+    </div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About us</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Location</a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.nav {
+  width: 100%;
+  padding: 20px 50px;
+  display: flex;
+}
+
+.nav-img {
+  width: 70px;
+}
+
+.nav-img,
+.nav,
+.title,
+.none,
+ul,
+a,
+li {
+  justify-content: space-between;
+  display: flex;
+  background-color: #fff;
+}
+ul {
+  display: flex;
+}
+a {
+  text-decoration: none;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  color: #000;
+  gap: 28px;
+  font-size: 16px;
+  margin-right: 26px;
+}
+.title {
+  align-items: center;
+  margin-left: 5px;
+  font-size: 26px;
+  font-weight: normal;
+  font-family: "Oranienbaum", serif;
+}
+</style>
