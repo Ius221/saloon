38af3f2e3d934b18fbc9a15d782c8e4d292e3a21
Ayush <ayush9783@gmail.com>
2025-07-05 23:42:01 +0530

    footer completed but responsive is left

--- src/App.vue
+++ src/App.vue
@@ -4,110 +4,120 @@
   </div>
 
   <the-header @mobile-menu-changed="handleMobileMenuChange" />
+  <route-view />
   <div class="main-section" :style="{ opacity: isMobileMenuOpen ? 0.5 : 1 }">
-    <h1>Hello from Vue + Bun!</h1>
+    <!-- <h1>Hello</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid
-      labore voluptas, voluptates officia modi. Dolorum consectetur deserunt
-      odio in repellat delectus necessitatibus quae, aperiam saepe. Ex sunt
-      corporis consectetur!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem neque maxime
-      aliquid illum recusandae nihil molestiae natus fugit nemo. Quisquam,
-      corrupti laboriosam accusantium sed a veniam vitae iusto debitis saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid
-      labore voluptas, voluptates officia modi. Dolorum consectetur deserunt
-      odio in repellat delectus necessitatibus quae, aperiam saepe. Ex sunt
-      corporis consectetur!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem neque maxime
-      aliquid illum recusandae nihil molestiae natus fugit nemo. Quisquam,
-      corrupti laboriosam accusantium sed a veniam vitae iusto debitis saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eos
-      ratione tenetur, laboriosam quidem a itaque reprehenderit fugiat nam
-      assumenda adipisci. At quaerat laudantium ratione obcaecati qui, mollitia
-      consequuntur corporis.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea officiis
-      dicta cumque dolorem quos, assumenda ab nobis voluptatibus hic provident
-      quod veritatis. Eveniet, velit ab atque voluptatem quidem iste modi!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid
-      labore voluptas, voluptates officia modi. Dolorum consectetur deserunt
-      odio in repellat delectus necessitatibus quae, aperiam saepe. Ex sunt
-      corporis consectetur!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem neque maxime
-      aliquid illum recusandae nihil molestiae natus fugit nemo. Quisquam,
-      corrupti laboriosam accusantium sed a veniam vitae iusto debitis saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eos
-      ratione tenetur, laboriosam quidem a itaque reprehenderit fugiat nam
-      assumenda adipisci. At quaerat laudantium ratione obcaecati qui, mollitia
-      consequuntur corporis.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea officiis
-      dicta cumque dolorem quos, assumenda ab nobis voluptatibus hic provident
-      quod veritatis. Eveniet, velit ab atque voluptatem quidem iste modi!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid
-      labore voluptas, voluptates officia modi. Dolorum consectetur deserunt
-      odio in repellat delectus necessitatibus quae, aperiam saepe. Ex sunt
-      corporis consectetur!
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem neque maxime
-      aliquid illum recusandae nihil molestiae natus fugit nemo. Quisquam,
-      corrupti laboriosam accusantium sed a veniam vitae iusto debitis saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eos
-      ratione tenetur, laboriosam quidem a itaque reprehenderit fugiat nam
-      assumenda adipisci. At quaerat laudantium ratione obcaecati qui, mollitia
-      consequuntur corporis.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea officiis
-      dicta cumque dolorem quos, assumenda ab nobis voluptatibus hic provident
-      quod veritatis. Eveniet, velit ab atque voluptatem quidem iste modi!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eos
-      ratione tenetur, laboriosam quidem a itaque reprehenderit fugiat nam
-      assumenda adipisci. At quaerat laudantium ratione obcaecati qui, mollitia
-      consequuntur corporis.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea officiis
-      dicta cumque dolorem quos, assumenda ab nobis voluptatibus hic provident
-      quod veritatis. Eveniet, velit ab atque voluptatem quidem iste modi!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos
-      doloremque sunt, consequatur ut repellat corrupti, exercitationem quisquam
-      magni fuga in, qui obcaecati soluta tempore! Ea commodi debitis dolorem
-      quis.
-    </p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolor
+      asperiores explicabo, quae similique eos error, incidunt nesciunt, natus
+      odio repudiandae! Architecto reprehenderit dolore quibusdam ipsum,
+      voluptatem recusandae natus at. Reiciendis suscipit vitae, eum laudantium
+      velit dolor voluptates? Ea cupiditate obcaecati deserunt eos ducimus,
+      recusandae repellendus vitae rem quo rerum dolor consectetur cumque
+      consequuntur quibusdam reprehenderit praesentium maiores mollitia hic!
+      Reprehenderit dignissimos qui neque quas laboriosam eligendi voluptates,
+      veniam ipsa. Vel in natus eos beatae iusto ipsum enim odit quia rem totam
+      consequatur aperiam veritatis aliquam doloremque nisi, sunt ad?
+      Exercitationem sapiente nisi asperiores voluptates natus officiis officia
+      repudiandae eligendi totam. Rem vero commodi asperiores doloribus, tenetur
+      omnis sit ipsum ipsa et, a, quia voluptatum quasi reiciendis? Enim, amet
+      doloremque. Ullam tempora tenetur nisi ea provident veniam fugit quo
+      reprehenderit dolor facere aliquid, quidem temporibus molestias magni
+      magnam distinctio natus iste atque quaerat eos labore libero quam aliquam.
+      Reprehenderit, tempora? Accusantium facere dolor est maxime, minima fugit
+      qui, adipisci esse velit eos necessitatibus amet laborum aliquid
+      praesentium deleniti nulla expedita sed exercitationem quo quos, rem
+      repudiandae asperiores? Tenetur, est officiis? Sint quaerat minima magni
+      dicta maiores sunt sit reiciendis eum dolore debitis nesciunt voluptate
+      corrupti laudantium et aut, praesentium eaque aperiam, veniam beatae
+      explicabo a dolorum? Minima, harum et? Corporis. Exercitationem fugiat
+      quam veniam quas qui perspiciatis? Quam iusto cupiditate iure. Nobis,
+      unde? Beatae fugiat hic distinctio reiciendis quo molestias magni.
+      Exercitationem in reiciendis sapiente nulla voluptate magnam et
+      consectetur! Blanditiis beatae tempore obcaecati rerum labore hic modi,
+      nesciunt soluta sequi aliquam ab perferendis fugiat reprehenderit neque
+      quasi deleniti accusamus. Et mollitia ex voluptas assumenda repellat
+      impedit facere, ratione repellendus. Fugiat aut nobis quas mollitia error,
+      dignissimos consequuntur atque suscipit aliquid, minima maiores qui
+      veritatis quam odit. Delectus perferendis sapiente odio saepe impedit,
+      natus laudantium voluptas, harum vitae omnis ad? Cumque et error explicabo
+      dolores placeat neque aliquam dolore. Ab officiis ut sed fuga, illum
+      laboriosam, hic deserunt, numquam laudantium error provident non
+      perspiciatis debitis repellendus libero doloremque odit voluptatem.
+      Voluptates culpa tempore vel ea iure consequatur cum saepe molestiae in
+      quam quod quisquam illum doloremque labore fugiat, praesentium fugit
+      architecto, maxime amet inventore esse, expedita odit excepturi. Cum,
+      beatae. Laboriosam adipisci fugiat dolorem ipsa nam quae illo aliquid
+      tempora itaque expedita amet sequi, rerum quis? Aperiam laboriosam
+      consequatur ullam, unde ipsum, labore exercitationem quasi nisi deserunt
+      adipisci nostrum numquam! Enim cumque ab aut obcaecati dolor? Veniam, ipsa
+      quas voluptate numquam commodi deserunt animi, voluptatem sapiente optio
+      obcaecati quaerat eveniet rem mollitia culpa, exercitationem incidunt
+      doloribus? Quidem ab eaque alias. Recusandae ut ad odit, rem ipsam eveniet
+      voluptate doloremque maiores soluta praesentium facere asperiores eaque
+      aut suscipit numquam voluptatum. Iure debitis cupiditate eos velit
+      reiciendis veniam tempore facere laborum? Amet! Sed, saepe ipsam aperiam
+      ad nemo in magnam reiciendis a, nam eum minima repellendus, nobis ducimus
+      ex consectetur distinctio quisquam! Incidunt eligendi delectus temporibus
+      inventore in facere quis corporis facilis. Quam perspiciatis suscipit
+      adipisci voluptatibus omnis consequuntur non dicta recusandae natus, illum
+      aspernatur magnam ipsam quae a, mollitia excepturi soluta tempore aut
+      minima asperiores beatae laudantium odit! Officiis, sit eos! Quos incidunt
+      minus repudiandae distinctio voluptatibus! Iste enim voluptate porro nulla
+      eaque rem totam pariatur minima nam nisi id animi nemo cum architecto eos
+      maxime illum, asperiores velit quos! Facilis. Assumenda, recusandae.
+      Sapiente officia mollitia illo nobis, odit nam facere error harum
+      consequatur laborum quod repellendus suscipit quae voluptate provident!
+      Earum consequuntur mollitia quia eius. Quibusdam veniam enim perspiciatis
+      quis! Necessitatibus doloremque facere enim nihil magni veniam fugiat
+      reprehenderit et aliquam dolores iste iure earum cum, a nisi autem dolore
+      natus voluptate, eum delectus quae provident molestias magnam odio?
+      Voluptates. Voluptates, pariatur impedit accusamus minus neque ipsam
+      eveniet explicabo minima reiciendis repellat libero. Quisquam, saepe!
+      Perferendis assumenda incidunt deserunt adipisci cum ducimus fuga voluptas
+      soluta omnis amet odio, nostrum iure. Voluptate, nulla, beatae soluta
+      nostrum totam quae, exercitationem necessitatibus voluptatum sed cumque
+      obcaecati asperiores eaque sapiente illo saepe maxime! Est maxime minus ea
+      amet, assumenda aliquam officiis dolore explicabo libero! Accusamus
+      aliquam molestiae repudiandae velit cupiditate tenetur pariatur provident,
+      libero, iure placeat eos. Eius blanditiis, qui repudiandae in dolorem
+      alias eum sit, vel velit corporis obcaecati, quasi deserunt? Eius,
+      deleniti! Sapiente voluptate ipsam repudiandae nemo dolorem ratione
+      voluptatibus nulla quibusdam aspernatur cumque eos mollitia cupiditate
+      eum, obcaecati tempore deleniti ullam aperiam adipisci temporibus
+      quisquam. Iusto dicta aspernatur sapiente eius quod! Fugiat libero
+      consequuntur, nesciunt pariatur recusandae numquam nam quidem, magnam
+      voluptatem molestiae, ad et accusamus soluta vero? Atque aut eveniet
+      itaque. Tempore reiciendis quasi pariatur praesentium illo repudiandae sed
+      autem! Cum dolorem quos voluptatum sit voluptatem. Pariatur dolorem
+      voluptate quis similique sunt blanditiis doloremque tenetur assumenda
+      magnam reiciendis ducimus saepe corrupti est quaerat minus cumque, natus
+      aliquid iure vitae! Eum? Libero accusantium aspernatur veritatis earum
+      ipsam quidem, error natus culpa ab quis pariatur, enim ad quasi ratione
+      vero rerum. Nesciunt nihil ex numquam corrupti sed temporibus libero
+      quaerat sequi aliquam. Nemo quis dolore cumque officiis sunt dolores ipsam
+      numquam odio nulla necessitatibus nostrum dolorem impedit placeat,
+      aspernatur incidunt ut voluptatem eligendi optio natus? Omnis, debitis
+      error non laborum corporis magnam! Ipsa non sunt nihil minima voluptatem
+      atque provident maiores ipsum esse facere fuga corrupti quidem totam
+      assumenda dolor, excepturi laudantium a illum impedit aspernatur
+      asperiores! Similique nostrum repellendus adipisci temporibus. Optio,
+      necessitatibus. Voluptas molestiae quas cumque quo velit, aspernatur
+      facere temporibus amet nulla, qui, minima dolor laboriosam corporis
+      delectus molestias fuga quam magnam architecto. Accusantium ab molestiae
+      ex ipsam similique.
+    </p> -->
   </div>
+  <the-footer />
 </template>
 
 <script>
-import TheHeader from "./components/Header/TheHeader.vue";
+import TheHeader from "./components/HeaderFooter/TheHeader.vue";
+import TheFooter from "./components/HeaderFooter/TheFooter.vue";
 export default {
   components: {
     TheHeader,
+    TheFooter,
   },
   data() {
     return {
---
+++ src/components/HeaderFooter/TheFooter.vue
@@ -0,0 +1,159 @@
+<template>
+  <div footer-div>
+    <div class="img">
+      <div class="img-div">
+        <div>
+          <img
+            src="../../assets/header/logo.png"
+            width="50px"
+            class="logo-img"
+          />
+        </div>
+        <p class="small-content">NEWSLETTER SIGNUP</p>
+        <p class="main-content">
+          <center>
+            Want to get updates on Hair <br />
+            & Wellness news?
+          </center>
+        </p>
+        <div class="email-field">
+          <input type="email" class="email-here" placeholder="Your Email" />
+          <p class="small-content">SUBSCRIBE</p>
+        </div>
+        <div class="list-item">
+          <ul>
+            <li v-for="(val, ind) in allList" :key="ind">
+              {{ val }}
+            </li>
+          </ul>
+        </div>
+        <div class="copyright">
+          <p>
+            © Copyright 2023
+            <span style="color: rgb(189, 118, 85); cursor: pointer"
+              >Scissors & Style</span
+            >. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      allList: [
+        "Home",
+        "About Us",
+        "Service",
+        "Our Projects",
+        "Blog",
+        "Contacts",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.img {
+  background-image: url("../../assets/header/footer.jpg");
+  font-family: "Montserrat", sans-serif;
+  background-repeat: no-repeat;
+  background-size: cover; /* Ensures image covers footer area */
+  background-position: center; /* Keeps image centered */
+  width: 100%;
+}
+ul {
+  display: flex;
+  gap: 35px;
+  padding: 14px 0;
+
+  font-weight: 500;
+  font-size: 14px;
+  text-transform: capitalize;
+  list-style-type: none;
+}
+li:hover {
+  color: rgb(189, 118, 85);
+  cursor: pointer;
+}
+.img-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  padding: 24px 0;
+  padding-bottom: 50vh;
+}
+.small-content {
+  letter-spacing: 2px;
+  color: rgb(189, 118, 85);
+  font-size: 12px;
+  font-weight: 600;
+}
+.logo-img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.main-content {
+  font-size: 40px;
+  text-transform: capitalize;
+  width: 50%;
+  padding: 15px 0;
+  font-family: "Oranienbaum", serif;
+}
+.email-field p {
+  width: 82.8906px;
+  height: 50px;
+  padding: 16px 0px;
+  display: inline-block;
+  box-sizing: border-box;
+  font-size: 12px;
+  font-family: Montserrat;
+  font-weight: 600;
+  text-align: center;
+  white-space: nowrap;
+  line-height: 18px;
+  color: rgb(34, 29, 26);
+  letter-spacing: 1.2px;
+  cursor: pointer;
+}
+.email-field input {
+  border: none;
+  outline: none; /* removes blue highlight on focus */
+  background-color: transparent;
+  width: 327.109px;
+  height: 52px;
+  padding: 13px 0px 19px;
+  display: inline-block;
+  box-sizing: border-box;
+  font-size: 14px;
+  font-family: Montserrat;
+  font-weight: 400;
+  line-height: 20px;
+  color: rgb(87, 83, 81);
+}
+.email-field {
+  border-bottom: 1px solid #000;
+  width: 410px;
+  height: 53px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-family: Montserrat;
+  align-items: center;
+  font-weight: 400;
+  line-height: 28px;
+  margin: 20px 0;
+  color: rgb(87, 83, 81);
+}
+.copyright {
+  margin-top: 10px;
+}
+</style>
